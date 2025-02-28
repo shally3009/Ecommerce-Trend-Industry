77c73445e93346912d1ff58fc010e486d478268a
shally3009 <mittalshally30@gmail.com>
2025-02-28 15:23:47 +0530

    done

--- backend/src/controller/Product.js
+++ backend/src/controller/Product.js
@@ -96,6 +96,33 @@ productrouter.get('/getcart',async(req,res)=>{
     }
 })
 
+productrouter.put('/edit-cart',async(req,res) => {
+    const {email,productid,quantity}=req.body
+
+    if(!email||productid||quantity==undefined){
+        return res.status(404).json({message:"put all details"})
+    }
+    const finduser=await userModel.findOne({email:email})
+    if(!finduser){
+        return res.status(500).json({message:"user is not found"})
+    }
+
+    const findproduct= await Productmodel.findOne({_id:productid})
+    if(!findproduct||findproduct.stock<=0){
+        return res.status(404).json({message:"product not available"})
+    }
+    const findcartproduct=finduser.cart.map(item=>item.productid===productid)
+
+    if(!findcartproduct){
+        return res.status(404).json({message:"can not find"})
+    }
+    findcartproduct.quantity=quantity
+    await finduser.save()
+    return res.status(200).json({message:"edited successfully"})
+}
+        
+})
+
 
 productrouter.post("/post-product", productupload.array('files'), async (req, res) => {
     const { name, price, description, category, stock, tags, email } = req.body;
