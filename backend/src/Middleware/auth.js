c1ad3732767cd5834363c7604f5ddcaea465105f
shally3009 <mittalshally30@gmail.com>
2025-03-05 10:58:49 +0530

    done

--- backend/src/Middleware/auth.js
+++ backend/src/Middleware/auth.js
@@ -1,3 +1,22 @@
 const ErrorHandler = require("../utils/ErrorHandler");
 const catchAsyncErrors = require("./catchAsyncErrors");
 const jwt = require("jsonwebtoken");
+require("dotenv").config({path:"./config/.env"});
+
+const auth =async(req,res,next)=>{
+   const tokenauth = req.headers.Authorization
+   const token=tokenauth.split(" ")[1]
+   const secret = process.env.secretkey;
+
+   jwt.verify(token,secret, function(err, decoded) {
+    if(err){
+        console.log{"error in auth middleware",err}
+    }
+    else{
+        next()
+    }
+  });
+    
+}
+
+module.exports = auth;
